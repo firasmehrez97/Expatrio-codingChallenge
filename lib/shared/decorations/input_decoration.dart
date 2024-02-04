97f34872f11a13e90fb32d2d3a1d32466fea3e68
Firas mehrez OSX <mehrez@sonah.tech>
2024-02-04 20:44:36 +0100

    refactor input decoration into one file

---
+++ lib/shared/decorations/input_decoration.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+InputDecoration inputDecoration = const InputDecoration(
+  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black),
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  errorBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black),
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  focusedErrorBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black),
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+);
+
+InputDecoration emailInputDecoration = const InputDecoration(
+  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black),
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  errorBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black),
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  focusedErrorBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black),
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+);
